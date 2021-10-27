9dd985a12e445f6e6ea87cc7d59db9da8f1686fe
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-10-27 11:16:54 +0200

    dao

--- t3/viabnb.tier3.dataserver.soap/src/main/java/dk/via/sw/sepgroup61/viabnb/tier3/dataserver/soap/dao/testmessage/TestMessageDAO.java
+++ t3/viabnb.tier3.dataserver.soap/src/main/java/dk/via/sw/sepgroup61/viabnb/tier3/dataserver/soap/dao/testmessage/TestMessageDAO.java
@@ -1,5 +1,11 @@
 package dk.via.sw.sepgroup61.viabnb.tier3.dataserver.soap.dao.testmessage;
 
+import dk.via.sw.sepgroup61.viabnb.tier3.dataserver.soap.models.ConceptMessage;
+
+import java.sql.SQLException;
+
 public interface TestMessageDAO
 {
+  ConceptMessage GetMessage(int msgId) throws SQLException;
+
 }
