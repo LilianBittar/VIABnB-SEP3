8d021990eb4607bab47ddc5ebbc359588922a318
Kasper <kasperscoop@gmail.com>
2021-10-27 10:48:12 +0200

    t2 basics

--- t1/SEP3BlazorT1Client/SEP3BlazorT1Client/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ t1/SEP3BlazorT1Client/SEP3BlazorT1Client/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,77 +13,77 @@ namespace SEP3BlazorT1Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Micha\Desktop\Skole\Software\3 SEM\SEP-3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
+#line 1 "C:\Users\Kasper\Git\SEP3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Micha\Desktop\Skole\Software\3 SEM\SEP-3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
+#line 2 "C:\Users\Kasper\Git\SEP3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Micha\Desktop\Skole\Software\3 SEM\SEP-3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
+#line 3 "C:\Users\Kasper\Git\SEP3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Micha\Desktop\Skole\Software\3 SEM\SEP-3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
+#line 4 "C:\Users\Kasper\Git\SEP3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Micha\Desktop\Skole\Software\3 SEM\SEP-3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
+#line 5 "C:\Users\Kasper\Git\SEP3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Micha\Desktop\Skole\Software\3 SEM\SEP-3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
+#line 6 "C:\Users\Kasper\Git\SEP3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Micha\Desktop\Skole\Software\3 SEM\SEP-3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
+#line 7 "C:\Users\Kasper\Git\SEP3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Micha\Desktop\Skole\Software\3 SEM\SEP-3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
+#line 8 "C:\Users\Kasper\Git\SEP3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Micha\Desktop\Skole\Software\3 SEM\SEP-3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
+#line 9 "C:\Users\Kasper\Git\SEP3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
 using SEP3BlazorT1Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Micha\Desktop\Skole\Software\3 SEM\SEP-3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
+#line 10 "C:\Users\Kasper\Git\SEP3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\_Imports.razor"
 using SEP3BlazorT1Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Micha\Desktop\Skole\Software\3 SEM\SEP-3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\Pages\FetchData.razor"
+#line 3 "C:\Users\Kasper\Git\SEP3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\Pages\FetchData.razor"
 using SEP3BlazorT1Client.Data;
 
 #line default
@@ -98,7 +98,7 @@ using SEP3BlazorT1Client.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 41 "C:\Users\Micha\Desktop\Skole\Software\3 SEM\SEP-3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\Pages\FetchData.razor"
+#line 41 "C:\Users\Kasper\Git\SEP3\VIABnB-SEP3\t1\SEP3BlazorT1Client\SEP3BlazorT1Client\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
