8fc37534796426152731ea34dd3800a86b4ac1f5
Michael Bui <michaelbui99@hotmail.com>
2021-11-05 10:16:44 +0100

    Added Spring Boot to project

--- t3/viabnb.tier3.dataserver.soapws/src/main/java/dk/viabnb/sep3/group6/dataserver/soapws/t3/dao/testmessage/TestMessageDao.java
+++ t3/viabnb.tier3.dataserver.soapws/src/main/java/dk/viabnb/sep3/group6/dataserver/soapws/t3/dao/testmessage/TestMessageDao.java
@@ -1,7 +1,5 @@
 package dk.viabnb.sep3.group6.dataserver.soapws.t3.dao.testmessage;
 
-import dk.viabnb.sep3.group6.dataserver.soapws.t3.models.ConceptMessage;
-
 public interface TestMessageDao
 {
   ConceptMessage GetMessage(int msgId);
