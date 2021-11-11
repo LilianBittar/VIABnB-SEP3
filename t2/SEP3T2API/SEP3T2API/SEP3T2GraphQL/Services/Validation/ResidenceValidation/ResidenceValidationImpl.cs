a6d0f534a94cc1cec29e6ca982b8e00cdc6e28be
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-11-11 16:44:07 +0100

    Changed the unit test to test the validation class instead of the service and repository Test runs successfully independent of the data server

--- t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/Validation/ResidenceValidation/ResidenceValidationImpl.cs
+++ t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/Validation/ResidenceValidation/ResidenceValidationImpl.cs
@@ -1,53 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using SEP3T2GraphQL.Models;
 
 namespace SEP3T2GraphQL.Services.Validation.ResidenceValidation
 {
     public class ResidenceValidationImpl : IResidenceValidation
     {
-        //TODO move thow and test here
         public bool IsValidAddress(Address address)
         {
-            return address != null && 
-                   ((address.Id != null && address.Id >= 0) &&
-                    (!string.IsNullOrEmpty(address.StreetName)) &&
-                    (!string.IsNullOrEmpty(address.HouseNumber)) &&
-                    (!string.IsNullOrEmpty(address.CityName) && IsLettersOnly(address.CityName)) &&
-                    (address.ZipCode is > 0 or <= 9999));
+            if (address != null && 
+                ((address.Id != null && address.Id >= 0) &&
+                 (!string.IsNullOrEmpty(address.StreetName)) &&
+                 (!string.IsNullOrEmpty(address.StreetNumber)) &&
+                 (!string.IsNullOrEmpty(address.HouseNumber)) &&
+                 (!string.IsNullOrEmpty(address.CityName) && IsLettersOnly(address.CityName)) &&
+                 ((address.ZipCode is >= 1000 and <= 9999) && address.ZipCode != null)))
+            {
+                return true;
+            }
+            throw new ArgumentException("Invalid address");
         }
 
         public bool IsValidRules(IList<Rule> rules)
         {
-            return rules != null && (rules.All(rule => (rule.Id >= 0) && !string.IsNullOrEmpty(rule.Description)));
+            if (rules != null && (rules.All(rule => (rule.Id >= 0) && !string.IsNullOrEmpty(rule.Description))))
+            {
+                return true;
+            }
+            throw new ArgumentException("Invalid rules");
         }
 
         public bool IsValidFacilities(IList<Facility> facilities)
         {
-            return facilities != null && (facilities.All(facility =>
-                (facility.Id >= 0) && !string.IsNullOrEmpty(facility.Name)));
+            if (facilities != null && 
+                (facilities.All(facility =>
+                (facility.Id >= 0) && 
+                (!string.IsNullOrEmpty(facility.Name))
+                )))
+            {
+                return true;
+            }
+            throw new ArgumentException("Invalid facilities");
         }
 
         public bool IsValidResidence(Residence residence)
         {
-            return (residence != null) &&
-                   ((residence.Id != null && residence.Id >= 0) &&
-                    (IsValidAddress(residence.Address)) &&
-                    (!string.IsNullOrEmpty(residence.Description)) &&
-                    (!string.IsNullOrEmpty(residence.Type)) &&
-                    ((residence.AverageRating >= 0) &&
-                     (residence.IsAvailable != null) &&
-                     (residence.PricePerNight >= 0) &&
-                     IsValidRules(residence.Rules) &&
-                     (IsValidFacilities(residence.Facilities)) &&
-                     (residence.AvailableFrom != null && residence.AvailableFrom >= DateTime.Now) &&
-                     (residence.AvailableTo != null && residence.AvailableTo >= DateTime.Now)));
+            if ((residence != null) &&
+                ((residence.Id != null && residence.Id >= 0) &&
+                 (IsValidAddress(residence.Address)) &&
+                 (!string.IsNullOrEmpty(residence.Description)) &&
+                 (!string.IsNullOrEmpty(residence.Type)) &&
+                 ((residence.AverageRating >= 0) &&
+                  (residence.IsAvailable != null) &&
+                  (residence.PricePerNight >= 0) &&
+                  IsValidRules(residence.Rules) &&
+                  (IsValidFacilities(residence.Facilities)) &&
+                  (residence.AvailableFrom != null && residence.AvailableFrom >= DateTime.Now) &&
+                  (residence.AvailableTo != null && residence.AvailableTo >= DateTime.Now))))
+            {
+                return true;
+            }
+            throw new ArgumentException("Invalid Residence");
         }
 
         public bool IsLettersOnly(string arg)
         {
-            return arg.Any(c => Char.IsLetter(c));
+            if (arg.All(char.IsLetter))
+            {
+                return true;
+            }
+            throw new ArgumentException($"Invalid string: {arg}");
+
         }
         
     }
