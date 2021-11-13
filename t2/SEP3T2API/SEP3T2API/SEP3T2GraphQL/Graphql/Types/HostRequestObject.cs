d0633620b00295ca1b6a913bbc0e7bc12d854aa3
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-11-13 17:45:56 +0100

    fix push error

---
+++ t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Graphql/Types/HostRequestObject.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using SEP3T2GraphQL.Models;
+
+namespace SEP3T2GraphQL.Graphql.Types
+{
+    public class HostRequestObject : ObjectGraphType<HostRegistrationRequest>
+    {
+        public HostRequestObject()
+        {
+            Name = nameof(HostRegistrationRequest);
+            Description = "HostRequest";
+
+            Field(h => h.Id).Description("Request id");
+            Field(h => h.PersonalImage).Description("Host image");
+            Field(h => h.CprNumber).Description("Host CPR number");
+            Field(h => h.Status).Description("Request staus");
+        }
+    }
+}
