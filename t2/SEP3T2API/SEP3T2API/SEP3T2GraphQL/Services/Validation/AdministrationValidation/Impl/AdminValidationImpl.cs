783de1f801e80d68c93c30de44f679dbeb37c7e7
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-11-13 16:53:47 +0100

    Administration for host t2 implemented

---
+++ t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/Validation/AdministrationValidation/Impl/AdminValidationImpl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SEP3T2GraphQL.Services.Validation.AdministrationValidation.Impl
+{
+    public class AdminValidationImpl : IAdminValidation
+    {
+        public bool IsValidId(int id)
+        {
+            if (id < 0)
+            {
+                throw new ArgumentException("Invalid ID");
+            }
+
+            return true;
+        }
+
+        public bool IsValidStatus(string status)
+        {
+            if (!status.Equals("Approved") || !status.Equals("NotApproved") || !status.Equals("NotAnswered"))
+            {
+                throw new ArgumentException("Invalid status");
+            }
+
+            return true;
+        }
+    }
+}
