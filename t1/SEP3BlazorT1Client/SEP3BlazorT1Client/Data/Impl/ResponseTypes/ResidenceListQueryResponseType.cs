245c0651352ccacfff4316df7d1e7aac8a18543a
Lil <bittarlily@gmail.com>
2021-11-14 15:28:18 +0100

    implemented GetResidenceByHostId method which called to GraphQl API

---
+++ t1/SEP3BlazorT1Client/SEP3BlazorT1Client/Data/Impl/ResponseTypes/ResidenceListQueryResponseType.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using SEP3BlazorT1Client.Models;
+
+namespace SEP3BlazorT1Client.Data.Impl.ResponseTypes
+{
+    public class ResidenceListQueryResponseType
+    {
+        [JsonProperty("Residence")]
+        public List<Residence> Residences { get; set; }
+    }
+}
