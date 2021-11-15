3a32c50cd7863089689b6b54ed26f916d2f4c428
Michael Bui <michaelbui99@hotmail.com>
2021-11-15 19:05:58 +0100

    Implemented CreateGuestRegistrationRequest for service in t1 client. Added gitignore to prevent binaries from being commited everytime

--- t1/SEP3BlazorT1Client/SEP3BlazorT1Client/Data/Impl/GraphQlAdministrationService.cs
+++ t1/SEP3BlazorT1Client/SEP3BlazorT1Client/Data/Impl/GraphQlAdministrationService.cs
@@ -1,14 +1,56 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using SEP3BlazorT1Client.Models;
+using CatQL.GraphQL.Client;
+using CatQLClient.QueryResponses;
+using SEP3BlazorT1Client.Data.Impl.ResponseTypes;
 
 namespace SEP3BlazorT1Client.Data.Impl
 {
+    //TODO: Maybe rename this service to UserService or RegistrationRequestService, since administration is maybe a bit vague? - Micmic
     public class GraphQlAdministrationService : IAdministrationService
     {
-        public Task<GuestRegistrationRequest> CreateGuestRegistrationRequestAsync(GuestRegistrationRequest guestRegistrationRequest)
+        private const string Url = "https://localhost:5001/graphql";
+        private GqlClient client = new GqlClient(Url); 
+        public async Task<GuestRegistrationRequest> CreateGuestRegistrationRequestAsync(GuestRegistrationRequest guestRegistrationRequest)
         {
-            throw new System.NotImplementedException();
+            var createGuestRegistrationRequestQuery = new GqlQuery()
+            {
+                Query = @"mutation ($request: GuestRegistrationRequestInput) {
+                          createGuestRegistrationRequest(guestRegistrationRequest: $request) {
+                            id,
+                            studentNumber,
+                            host {
+                              id,
+                              firstName,
+                              lastName,
+                              isApprovedHost,
+                              phoneNumber,
+                              password,
+                              hostReviews {
+                                id,
+                                rating,
+                                text,
+                                viaId
+                              },
+                              profileImageUrl
+                            },
+                            studentIdImage,
+                            status
+                          }
+                        }
+                        ",
+                Variables = new {request = guestRegistrationRequest}
+            };
+            var response = await client.PostQueryAsync<GuestRegistrationRequestResponse>(createGuestRegistrationRequestQuery);
+
+            if (response.Errors != null)
+            {
+                throw new Exception("Something went wrong, try again later..."); 
+            }
+
+            return response.Data.CreateGuestRegistrationRequest; 
         }
 
         public Task<IEnumerable<GuestRegistrationRequest>> GetAllGuestRegistrationRequestsAsync()
---
+++ t1/SEP3BlazorT1Client/SEP3BlazorT1Client/Data/Impl/ResponseTypes/GuestRegistrationRequestResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using SEP3BlazorT1Client.Models;
+
+namespace SEP3BlazorT1Client.Data.Impl.ResponseTypes
+{
+    class GuestRegistrationRequestResponse
+    {
+        [JsonProperty("createGuestRegistrationRequest")]
+        public GuestRegistrationRequest CreateGuestRegistrationRequest { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
