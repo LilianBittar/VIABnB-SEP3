b4134dfd30207f45e2b0c0435f4a2fefb62b2252
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-11-15 21:57:43 +0100

    documentation

--- t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Repositories/Administration/IAdministrationRepository.cs
+++ t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Repositories/Administration/IAdministrationRepository.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using SEP3T2GraphQL.Models;
 
+//Todo next sprint check about the IsValid methods. should be split or just change the approach. NOTE: change in t1 and t3 after
 namespace SEP3T2GraphQL.Repositories.Administration
 {
     /// <summary>
@@ -18,17 +19,48 @@ namespace SEP3T2GraphQL.Repositories.Administration
         /// <returns>List of all HostRegistrationRequest objects in the system</returns>
         Task<IList<HostRegistrationRequest>> GetAllHostRegistrationRequests();
         /// <summary>
-        /// Method that returns a host requests based on the given parameter
+        /// Method that returns a host request based on the given parameter
         /// </summary>
         /// <param name="hostId">The Id of the host object that created the request</param>
-        /// <returns>HostRegistrationRequest objects </returns>
+        /// <returns>HostRegistrationRequest object</returns>
         Task<HostRegistrationRequest> GetHostRegistrationRequestByHostId(int hostId);
+        /// <summary>
+        /// Method that returns a host request based on the given parameter
+        /// </summary>
+        /// <param name="requestId">The Id of the desired host request</param>
+        /// <returns>HostRegistrationRequest object</returns>
         Task<HostRegistrationRequest> GetHostRegistrationRequestById(int requestId);
+        /// <summary>
+        /// Method that validates the host profile using that host's Id and changing its status
+        /// </summary>
+        /// <param name="requestId">The desired host's Id</param>
+        /// <param name="status">The new status of the host</param>
+        /// <returns></returns>
         Task IsValidHost(int requestId, RequestStatus status);
         //The below code relevant for handling the GuestRegistrationRequest
+        /// <summary>
+        /// Method that returns a list of all guest requests in the system 
+        /// </summary>
+        /// <returns>List of all GuestRegistrationRequest objects in the system</returns>
         Task<IList<GuestRegistrationRequest>> GetAllGuestRegistrationRequests();
+        /// <summary>
+        /// Method that returns a guest request based on the given parameter
+        /// </summary>
+        /// <param name="hostId">The Id of the host object that created the request</param>
+        /// <returns>GuestRegistrationRequest object</returns>
         Task<GuestRegistrationRequest> GetGuestRegistrationRequestByHostId(int hostId);
+        /// <summary>
+        /// Method that returns a guest request based on the given parameter
+        /// </summary>
+        /// <param name="requestId">The Id of the desired guest request</param>
+        /// <returns>HostRegistrationRequest object</returns>
         Task<GuestRegistrationRequest> GetGuestRegistrationRequestById(int requestId);
+        /// <summary>
+        /// Method that validates the host profile using that host's Id and changing its status
+        /// </summary>
+        /// <param name="requestId">The desired guest's Id</param>
+        /// <param name="status">The new status of the guest</param>
+        /// <returns></returns>
         Task IsValidGuest(int requestId, RequestStatus status);
     }
 }
