4ebadd608069c94d650ccc822e1113733c8bfaf4
Michael Bui <michaelbui99@hotmail.com>
2021-11-16 21:54:51 +0100

    Added unit test for GuestRegistrationController method CreateGuestRegistrationRequest

--- t3/viabnb.tier3.dataserver.soapws/src/main/test/dk/viabnb/sep3/group6/dataserver/rest/t3/controllers/GuestRegistrationControllerTest.java
+++ t3/viabnb.tier3.dataserver.soapws/src/main/test/dk/viabnb/sep3/group6/dataserver/rest/t3/controllers/GuestRegistrationControllerTest.java
@@ -1,7 +1,17 @@
 package dk.viabnb.sep3.group6.dataserver.rest.t3.controllers;
 
+import static org.mockito.Mockito.*;
+
 import dk.viabnb.sep3.group6.dataserver.rest.t3.dao.guestregistrationrequest.GuestRegistrationRequestDAO;
+import dk.viabnb.sep3.group6.dataserver.rest.t3.models.GuestRegistrationRequest;
+import dk.viabnb.sep3.group6.dataserver.rest.t3.models.Host;
+import dk.viabnb.sep3.group6.dataserver.rest.t3.models.HostReview;
+import dk.viabnb.sep3.group6.dataserver.rest.t3.models.RequestStatus;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,8 +20,17 @@ class GuestRegistrationControllerTest {
     private GuestRegistrationController controller;
 
     @BeforeEach
-    private void setup(){
-
+     void setup() {
+        guestRegistrationRequestDAO = mock(GuestRegistrationRequestDAO.class);
         controller = new GuestRegistrationController(guestRegistrationRequestDAO);
+    }
+
+    @Test
+     void ControllerReturnsInternalServerErrorWhenRepositoryReturnsNull() {
+        Host host = new Host(1, "test", "test", "test", "test@test.test", "test", new ArrayList<HostReview>(), "test", true);
+        GuestRegistrationRequest request = new GuestRegistrationRequest(1, 293886, "test", RequestStatus.NotAnswered);
+        when(guestRegistrationRequestDAO.createGuestRegistrationRequest(request)).thenReturn(null);
+        ResponseEntity<GuestRegistrationRequest> response = controller.createGuestRegistrationRequest(request);
+        assertEquals(ResponseEntity.internalServerError().build(), response);
     }
 }
