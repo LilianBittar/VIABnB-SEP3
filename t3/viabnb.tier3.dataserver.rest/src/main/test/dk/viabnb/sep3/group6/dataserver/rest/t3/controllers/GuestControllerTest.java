7a04e35635583e683c500ebdd51468106a22ea20
Michael Bui <michaelbui99@hotmail.com>
2021-11-17 21:01:39 +0100

    renamed test

--- t3/viabnb.tier3.dataserver.rest/src/main/test/dk/viabnb/sep3/group6/dataserver/rest/t3/controllers/GuestControllerTest.java
+++ t3/viabnb.tier3.dataserver.rest/src/main/test/dk/viabnb/sep3/group6/dataserver/rest/t3/controllers/GuestControllerTest.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 
-class GuestRegistrationControllerTest {
+class GuestControllerTest {
     private GuestDAO guestDAO;
     private GuestController controller;
 
