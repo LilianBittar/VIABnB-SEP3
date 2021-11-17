a648c6b8c0e493bd2d4613e2c72ac7da37a41076
Lil <bittarlily@gmail.com>
2021-11-17 11:30:29 +0100

    implemented DAO for guest and host authentication in t3

--- t3/viabnb.tier3.dataserver.soapws/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/dao/authentication/HostAuthenticationJsonDAO.java
+++ t3/viabnb.tier3.dataserver.soapws/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/dao/authentication/HostAuthenticationJsonDAO.java
@@ -3,8 +3,11 @@ package dk.viabnb.sep3.group6.dataserver.rest.t3.dao.authentication;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import dk.viabnb.sep3.group6.dataserver.rest.t3.models.GuestAuthenticationRequest;
+
+import dk.viabnb.sep3.group6.dataserver.rest.t3.models.Host;
 import dk.viabnb.sep3.group6.dataserver.rest.t3.models.HostAuthenticationRequest;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -12,12 +15,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.NoSuchElementException;
 
+@Repository @Scope("singleton")
 public class HostAuthenticationJsonDAO implements HostAuthenticationDAO{
-    private final static String REQUESTS_FILE = "guestRegReq.json";
-    private List<HostAuthenticationRequest> allRequests;
+
+    private final static String REQUESTS_FILE = "host.json";
+    private List<Host> hosts;
     private Gson gson = new GsonBuilder().serializeNulls().create();
 
     public HostAuthenticationJsonDAO(){
@@ -27,7 +33,7 @@ public class HostAuthenticationJsonDAO implements HostAuthenticationDAO{
             try
             {
                 file.createNewFile();
-                allRequests = new ArrayList<>();
+                hosts = new ArrayList<>();
             }
             catch (IOException e)
             {
@@ -43,10 +49,10 @@ public class HostAuthenticationJsonDAO implements HostAuthenticationDAO{
                 String requestAsJson = "";
                 while ((requestAsJson = reader.readLine()) != null)
                 {
-                    Type type = new TypeToken<ArrayList<HostAuthenticationRequest>>()
+                    Type type = new TypeToken<ArrayList<Host>>()
                     {
                     }.getType();
-                    allRequests = gson.fromJson(
+                    hosts = gson.fromJson(
                             requestAsJson, type);
                 }
             }
@@ -57,13 +63,25 @@ public class HostAuthenticationJsonDAO implements HostAuthenticationDAO{
         }
     }
 
+    @Override
+    public Host GetHostByEmail(String email){
+        for (int i = 0; i < hosts.size(); i++){
+            if(hosts.get(i).getEmail().equals(email)){
+                return hosts.get(i);
+            }
+        }
+        return null;
+    }
 
     @Override
-    public HostAuthenticationRequest AuthenticateHost(HostAuthenticationRequest request) {
-        HostAuthenticationRequest existingRequest = allRequests.get(allRequests.indexOf(request));
-        if (existingRequest == null){
-            throw new NoSuchElementException();
+    public Host GetHostById(int Id){
+        for (int i = 0; i < hosts.size(); i++){
+            if(hosts.get(i).getId()==Id){
+                return hosts.get(i);
+            }
         }
-        return existingRequest;
+        return null;
     }
+
+
 }
