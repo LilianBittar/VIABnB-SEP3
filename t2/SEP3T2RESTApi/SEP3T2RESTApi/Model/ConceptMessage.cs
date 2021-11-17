6cee21a02ad1c12a75d53095ef8d0f8322113953
Michael Bui <michaelbui99@hotmail.com>
2021-11-17 13:12:06 +0100

    Deleted unused projects

--- t2/SEP3T2RESTApi/SEP3T2RESTApi/Model/ConceptMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SEP3T2RESTApi.Model
-{
-    public class ConceptMessage
-    {
-        public int Id { get; set; }
-        public string Message { get; set; }
-        
-    }
-}
--- t3/viabnb.tier3.dataserver.soap/src/main/java/dk/via/sw/sepgroup61/viabnb/tier3/dataserver/soap/util/PostgresUsers.java
+++
@@ -1,9 +0,0 @@
-package dk.via.sw.sepgroup61.viabnb.tier3.dataserver.soap.util;
-
-public class PostgresUsers
-{
-  public static final String MICHAEL = "MichaelDatabasePassword";
-  public static final String KUTAIBA = "sW20@21kt";
-  public static final String LILIAN = "password";
-  public static final String KASPER = "hbb54egnn";
-}
