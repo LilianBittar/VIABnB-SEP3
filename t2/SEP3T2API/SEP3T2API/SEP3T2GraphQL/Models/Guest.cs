7da31e057904b6c1d38777648252ba1796133b21
Michael Bui <michaelbui99@hotmail.com>
2021-11-19 14:32:02 +0100

    Added valdation of student number to GuestValidator

--- t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Models/Guest.cs
+++ t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Models/Guest.cs
@@ -6,7 +6,6 @@ namespace SEP3T2GraphQL.Models
     public class Guest : Host
     {
         [Required] 
-        [Range(100000, 999999, ErrorMessage = "Invalid student number")]
         public int ViaId { get; set; }
         public IList<GuestReview> GuestReviews { get; set; } = new List<GuestReview>();
         public bool IsApprovedGuest { get; set; } = false; 
--- t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/Validation/GuestValidator.cs
+++ t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/Validation/GuestValidator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Net.Mail;
 using SEP3T2GraphQL.Models;
 
@@ -19,12 +20,13 @@ namespace SEP3T2GraphQL.Services.Validation
             List<ValidationResult> validationResults = new();
             bool isValidGuest = Validator.TryValidateObject(guest, context, validationResults, true);
             ValidateEmail(guest);
+            ValidateStudentNumber(guest); 
             if (!isValidGuest)
             {
-                throw new ArgumentException(validationResults[0].ErrorMessage); 
+                throw new ArgumentException(validationResults[0].ErrorMessage);
             }
         }
-        
+
         /// <summary>
         /// Validates an email of an guest. Checks if the emails contains @ or if the emails ends with a dot.
         /// </summary>
@@ -35,17 +37,36 @@ namespace SEP3T2GraphQL.Services.Validation
         {
             if (guest.Email.Trim().EndsWith("."))
             {
-                throw new ArgumentException("Invalid email"); 
+                throw new ArgumentException("Invalid email");
             }
+
             try
             {
                 var mailAddress = new MailAddress(guest.Email);
             }
             catch
             {
-                throw new ArgumentException("Invalid email"); 
+                throw new ArgumentException("Invalid email");
             }
         }
-        
+
+        /// <summary>
+        /// Validates the student number of the guest.
+        /// </summary>
+        /// <param name="guest">The guest who's student number is to be validated</param>
+        /// <exception cref="ArgumentException">student number is negative</exception>
+        /// <exception cref="ArgumentException">student number is not 6 digits</exception>
+        private static void ValidateStudentNumber(Guest guest)
+        {
+            var studentNumberAsString = guest.ViaId.ToString();
+            if (studentNumberAsString.Length != 6)
+            {
+                throw new ArgumentException("Invalid student number");
+            }
+            if (guest.ViaId < 0)
+            {
+                throw new ArgumentException("Invalid student number");
+            }
+        }
     }
 }
