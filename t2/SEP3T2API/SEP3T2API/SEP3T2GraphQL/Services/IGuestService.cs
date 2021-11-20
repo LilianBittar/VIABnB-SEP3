589f8e135c7bc85803cf5e34829274461b8397e5
Michael Bui <michaelbui99@hotmail.com>
2021-11-20 14:26:05 +0100

    Added xml doc for CreateGuest in IGuestService

--- t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/IGuestService.cs
+++ t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/IGuestService.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using SEP3T2GraphQL.Models;
+using SEP3T2GraphQL.Services.Validation;
 
 namespace SEP3T2GraphQL.Services
 {
     public interface IGuestService
     {
+        /// <summary>
+        /// Creates a new guest
+        /// </summary>
+        /// <param name="guest">the guest to be created</param>
+        /// <exception cref="ArgumentException">If guest is null</exception>
+        /// <exception cref="KeyNotFoundException">If host does not exist in the system</exception>
+        /// <exception cref="ArgumentException">If guest is invalid <see cref="GuestValidator"/></exception>
+        /// <returns>the created guest</returns>
         Task<Guest> CreateGuestAsync(Guest guest);
         Task<Guest> GetGuestById(int id);
         Task<Guest> GetGuestByEmail(string email);
