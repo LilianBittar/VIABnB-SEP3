dc7d70cb605720a65d7a982d30b5feb2fc2e0927
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-11-22 21:53:50 +0100

    bug fix

--- t3/viabnb.tier3.dataserver.rest/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/controllers/HostController.java
+++ t3/viabnb.tier3.dataserver.rest/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/controllers/HostController.java
@@ -18,88 +18,94 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-@RestController
-public class HostController
+@RestController public class HostController
 {
-    private static final Logger LOGGER= LoggerFactory.getLogger(HostController.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(
+      HostController.class);
 
-    // TODO: rename the paths to use /hosts/... as that is the standard for RESTful collections.
-    private HostDAO hostDAO;
-    private Gson gson = new GsonBuilder().serializeNulls().create();
+  // TODO: rename the paths to use /hosts/... as that is the standard for RESTful collections.
+  private HostDAO hostDAO;
+  private Gson gson = new GsonBuilder().serializeNulls().create();
 
-    @Autowired
-    public HostController(/*@Qualifier("HostDAOImpl") */HostDAO hostDAO)
-    {
-        this.hostDAO = hostDAO;
-    }
+  @Autowired public HostController(/*@Qualifier("HostDAOImpl") */
+      HostDAO hostDAO)
+  {
+    this.hostDAO = hostDAO;
+  }
 
-    @PostMapping("/host")
-    public ResponseEntity<Host> createHost(@RequestBody Host host  )
+  @PostMapping("/host") public ResponseEntity<Host> createHost(
+      @RequestBody Host host)
+  {
+    Host newHost = hostDAO.RegisterHost(host);
+    if (newHost == null)
     {
-        Host newHost = hostDAO.RegisterHost(host);
-        if (newHost == null)
-        {
-            return ResponseEntity.internalServerError().build();
-        }
-        return new ResponseEntity<>(newHost, HttpStatus.OK);
+      return ResponseEntity.internalServerError().build();
     }
-//TODO: This is conflicting with the handler for /host/{id}. Move this into a getAllHosts method and make email and query param instead. -micmic2
-//    @GetMapping("/host/{email}")
-//    public ResponseEntity<Host> getHostByEmail(@PathVariable String email)
-//    {
-//        Host host;
-//        host = hostDAO.getHostByEmail(email);
-//        if (host == null){
-//            return ResponseEntity.internalServerError().build();
-//        }
-//        return new ResponseEntity<>(host, HttpStatus.OK);
-//    }
-    @GetMapping("/host/{id}")
-    public ResponseEntity<Host> getHostById(@PathVariable int id)
-    {
-        Host host;
-        host = hostDAO.getHostById(id);
-        //Host can be null, because of the host can just not exist in the system. Does not need to be a server error that caused it. -micmic
-        //Better to just return the null, and let t2 handle the business logic. -micmic
-//        if (host == null){
-//            return ResponseEntity.internalServerError().build();
-//        }
-        LOGGER.info("Request for: " + gson.toJson(host));
-        return new ResponseEntity<>(host, HttpStatus.OK);
-    }
+    return new ResponseEntity<>(newHost, HttpStatus.OK);
+  }
 
-    @GetMapping("/hosts/notApproved")
-    public ResponseEntity<List<Host>> getAllNotApprovedHosts()
-    {
-        List<Host> hostsToReturn;
-        hostsToReturn = hostDAO.getAllNotApprovedHosts();
-        if (hostsToReturn == null)
-        {
-            return ResponseEntity.internalServerError().build();
-        }
-        return new ResponseEntity<>(hostsToReturn, HttpStatus.OK);
-    }
+  //TODO: This is conflicting with the handler for /host/{id}. Move this into a getAllHosts method and make email and query param instead. -micmic2
+  //    @GetMapping("/host/{email}")
+  //    public ResponseEntity<Host> getHostByEmail(@PathVariable String email)
+  //    {
+  //        Host host;
+  //        host = hostDAO.getHostByEmail(email);
+  //        if (host == null){
+  //            return ResponseEntity.internalServerError().build();
+  //        }
+  //        return new ResponseEntity<>(host, HttpStatus.OK);
+  //    }
+  @GetMapping("/host/{id}") public ResponseEntity<Host> getHostById(
+      @PathVariable int id)
+  {
+    Host host;
+    host = hostDAO.getHostById(id);
+    //Host can be null, because of the host can just not exist in the system. Does not need to be a server error that caused it. -micmic
+    //Better to just return the null, and let t2 handle the business logic. -micmic
+    //        if (host == null){
+    //            return ResponseEntity.internalServerError().build();
+    //        }
+    LOGGER.info("Request for: " + gson.toJson(host));
+    return new ResponseEntity<>(host, HttpStatus.OK);
+  }
 
-    @PatchMapping("/host/{id}/approval")
-    public ResponseEntity<Host> updateHostStatus(@RequestBody Host host, @RequestParam("id") int id )
+  @GetMapping("/hosts/notApproved") public ResponseEntity<List<Host>> getAllNotApprovedHosts()
+  {
+    List<Host> hostsToReturn;
+    hostsToReturn = hostDAO.getAllNotApprovedHosts();
+    if (hostsToReturn == null)
     {
-        try
-        {
-            if (host.isApprovedHost())
-            {
-                Host approvedHost = hostDAO.approveHost(host);
-                return ResponseEntity.ok(approvedHost);
-            }
-            else if (!host.isApprovedHost())
-            {
-                Host rejectedHost = hostDAO.rejectHost(host);
-                return ResponseEntity.ok(rejectedHost);
-            }
-            return ResponseEntity.badRequest().build();
-        }
-        catch (NoSuchElementException e)
-        {
-            return ResponseEntity.notFound().build();
-        }
+      return ResponseEntity.internalServerError().build();
     }
+    return new ResponseEntity<>(hostsToReturn, HttpStatus.OK);
+  }
+
+  //@PatchMapping("/host/{id}/approval")
+  @RequestMapping(value = "/host/{id}/approval", produces = "application/json", method = RequestMethod.PATCH) public ResponseEntity<Host> updateHostStatus(
+      @RequestBody Host host, /*@RequestParam("id")*/
+      @PathVariable("id") int id)
+  {
+    LOGGER.info("Recived updated host from t2: " + new Gson().toJson(host));
+    Host updatedHost = null;
+    try
+    {
+      if (host.isApprovedHost())
+      {
+        updatedHost = hostDAO.approveHost(host);
+        return ResponseEntity.ok(updatedHost);
+      }
+      else if (!host.isApprovedHost())
+      {
+        updatedHost = hostDAO.rejectHost(host);
+        return ResponseEntity.ok(updatedHost);
+      }
+      return ResponseEntity.badRequest().build();
+    }
+    catch (NoSuchElementException e)
+    {
+      e.printStackTrace();
+      LOGGER.error("Failed to execute update" + e.getMessage());
+      return ResponseEntity.notFound().build();
+    }
+  }
 }
