f142865a7bfa1b7699f64258fdb230bbdbfbacfe
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-11-23 20:09:18 +0100

    t3 address split into Address and City

---
+++ t3/viabnb.tier3.dataserver.rest/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/models/City.java
@@ -0,0 +1,51 @@
+package dk.viabnb.sep3.group6.dataserver.rest.t3.models;
+
+public class City
+{
+  private int cityId;
+  private String cityName;
+  private int zipCode;
+
+  public City(int cityId, String cityName, int zipCode)
+  {
+    this.cityId = cityId;
+    this.cityName = cityName;
+    this.zipCode = zipCode;
+  }
+
+  public int getCityId()
+  {
+    return cityId;
+  }
+
+  public void setCityId(int cityId)
+  {
+    this.cityId = cityId;
+  }
+
+  public String getCityName()
+  {
+    return cityName;
+  }
+
+  public void setCityName(String cityName)
+  {
+    this.cityName = cityName;
+  }
+
+  public int getZipCode()
+  {
+    return zipCode;
+  }
+
+  public void setZipCode(int zipCode)
+  {
+    this.zipCode = zipCode;
+  }
+
+  @Override public String toString()
+  {
+    return "City{" + "cityId=" + cityId + ", cityName='" + cityName + '\''
+        + ", zipCode=" + zipCode + '}';
+  }
+}
