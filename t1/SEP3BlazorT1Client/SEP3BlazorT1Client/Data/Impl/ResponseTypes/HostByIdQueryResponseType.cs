a4184afe999a2beee7303b2d535f3b688389c6e7
Michael Bui <michaelbui99@hotmail.com>
2021-11-24 00:49:31 +0100

    Added new response type

---
+++ t1/SEP3BlazorT1Client/SEP3BlazorT1Client/Data/Impl/ResponseTypes/HostByIdQueryResponseType.cs
@@ -0,0 +1,13 @@
+using Newtonsoft.Json;
+using SEP3BlazorT1Client.Models;
+
+namespace SEP3BlazorT1Client.Data.Impl.ResponseTypes
+{
+    public class HostByIdQueryResponseType
+    {
+        [JsonProperty("hostById")]
+        public Host HostById { get; set; }
+
+
+    }
+}
