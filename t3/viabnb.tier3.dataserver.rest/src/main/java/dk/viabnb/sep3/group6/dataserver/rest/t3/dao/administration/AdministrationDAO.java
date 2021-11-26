a0ed78a86ea937e0c72e9e9bd00eefa738256a62
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-11-26 11:46:34 +0100

    Get all admins end point added to admin controler

--- t3/viabnb.tier3.dataserver.rest/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/dao/administration/AdministrationDAO.java
+++ t3/viabnb.tier3.dataserver.rest/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/dao/administration/AdministrationDAO.java
@@ -2,7 +2,10 @@ package dk.viabnb.sep3.group6.dataserver.rest.t3.dao.administration;
 
 import dk.viabnb.sep3.group6.dataserver.rest.t3.models.Administrator;
 
+import java.util.List;
+
 public interface AdministrationDAO
 {
   Administrator getAdministratorByEmail(String email);
+  List<Administrator> getAllAdministrators();
 }
--- t3/viabnb.tier3.dataserver.rest/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/dao/residence/ResidenceDAOImpl.java
+++ t3/viabnb.tier3.dataserver.rest/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/dao/residence/ResidenceDAOImpl.java
@@ -15,56 +15,60 @@ import java.util.ArrayList;
 import java.util.List;
 //TODO: We need ResidenceRating model class and DAO to implement this
 
-public class ResidenceDAOImpl extends BaseDao implements ResidenceDAO {
-    private AddressDAO addressDAO = new AddressDAOImpl();
+public class ResidenceDAOImpl extends BaseDao implements ResidenceDAO
+{
+  private AddressDAO addressDAO = new AddressDAOImpl();
 
-    @Override
-    public Residence getByResidenceId(int id) {
-        return null;
-    }
+  @Override public Residence getByResidenceId(int id)
+  {
+    return null;
+  }
 
-    @Override
-    public List<Residence> getAllResidenceByHostId(int id) {
-        return null;
-    }
+  @Override public List<Residence> getAllResidenceByHostId(int id)
+  {
+    return null;
+  }
 
-    @Override
-    public Residence createResidence(Residence residence)
+  @Override public Residence createResidence(Residence residence)
+  {
+    try (Connection connection = getConnection())
     {
-        try(Connection connection = getConnection())
-        {
-            PreparedStatement stm = connection.prepareStatement
-                ("INSERT INTO residence(addressid, type, description, isavailable, priceprnight, availablefrom, availableto, imageurl, hostid) VALUES(?,?,?,?,?,?,?,?,?)");
-            stm.setInt(1, residence.getAddress().getId());
-            stm.setString(2, residence.getType());
-            stm.setString(3, residence.getDescription());
-            stm.setBoolean(4, residence.isAvailable());
-            stm.setDouble(5, residence.getPricePerNight());
-            stm.setDate(6, (Date) residence.getAvailableFrom());
-            stm.setDate(7, (Date) residence.getAvailableTo());
-            stm.setString(8, residence.getImageURL());
-            stm.setInt(9, residence.getHost().getId());
-            stm.executeUpdate();
-            connection.commit();
-            return residence;
-        }
-        catch (SQLException throwables)
-        {
-            throw new IllegalArgumentException(throwables.getMessage());
-        }
+      PreparedStatement stm = connection.prepareStatement(
+          "INSERT INTO residence(addressid, type, description, isavailable, priceprnight, availablefrom, availableto, imageurl, hostid) VALUES(?,?,?,?,?,?,?,?,?)");
+      stm.setInt(1, residence.getAddress().getId());
+      stm.setString(2, residence.getType());
+      stm.setString(3, residence.getDescription());
+      stm.setBoolean(4, residence.isAvailable());
+      stm.setDouble(5, residence.getPricePerNight());
+      stm.setDate(6, (Date) residence.getAvailableFrom());
+      stm.setDate(7, (Date) residence.getAvailableTo());
+      stm.setString(8, residence.getImageURL());
+      stm.setInt(9, residence.getHost().getId());
+      stm.executeUpdate();
+      connection.commit();
+      return residence;
     }
+    catch (SQLException throwables)
+    {
+      throw new IllegalArgumentException(throwables.getMessage());
+    }
+  }
 
-    @Override
-    public List<Residence> getAllResidences() throws IllegalStateException {
-        try (Connection connection = getConnection()) {
-            PreparedStatement stm = connection.prepareStatement("SELECT * from residence" +
-                    " join address a on a.addressid = residence.addressid " +
-                    "join city c on c.cityid = a.cityid " +
-                    "join residencefacility r on residence.residenceid = r.residenceid " +
-                    "join rule r2 on residence.residenceid = r2.residenceid" +
-                    " join host h on residence.hostid = h.hostid" );
-            ResultSet result = stm.executeQuery();
-            while (result.next()){
+  @Override public List<Residence> getAllResidences()
+      throws IllegalStateException
+  {
+    try (Connection connection = getConnection())
+    {
+      PreparedStatement stm = connection.prepareStatement(
+          "SELECT * from residence"
+              + " join address a on a.addressid = residence.addressid "
+              + "join city c on c.cityid = a.cityid "
+              + "join residencefacility r on residence.residenceid = r.residenceid "
+              + "join rule r2 on residence.residenceid = r2.residenceid"
+              + " join host h on residence.hostid = h.hostid");
+      ResultSet result = stm.executeQuery();
+      while (result.next())
+      {
                 /*TODO: 1. Create Host from query result.
                         2. Create City from query result.
                         3. Create Address from query result.
@@ -74,84 +78,79 @@ public class ResidenceDAOImpl extends BaseDao implements ResidenceDAO {
                         Might have to split into multiple queries to get the list of rules and facilities?
                 */
 
-            }
-        } catch (SQLException throwables) {
-            throw new IllegalStateException(throwables.getMessage());
-        }
-
-        return null;
+      }
     }
-
-    private List<Rule> getRulesByResidenceId(int residenceId)
+    catch (SQLException throwables)
     {
-        List<Rule> ruleListToReturn = new ArrayList<>();
-        try(Connection connection = getConnection())
-        {
-            PreparedStatement stm = connection.prepareStatement
-                ("SELECT * FROM rule WHERE residenceid = ?");
-            stm.setInt(1, residenceId);
-            ResultSet result = stm.executeQuery();
-            while (result.next())
-            {
-                Rule rule = new Rule
-                    (
-                        result.getString("ruledescription"),
-                        result.getInt("residenceid")
-                    );
-                ruleListToReturn
-                    .add(rule);
-            }
-            return ruleListToReturn;
-        }
-        catch (SQLException throwables)
-        {
-            throw new IllegalArgumentException(throwables.getMessage());
-        }
+      throw new IllegalStateException(throwables.getMessage());
     }
 
-    private Facility getFacilityById(int facilityId)
-    {
-        try(Connection connection = getConnection())
-        {
-            PreparedStatement stm = connection.prepareStatement
-                ("SELECT * FROM facility WHERE facilityid = ?");
-            stm.setInt(1, facilityId);
-            ResultSet result = stm.executeQuery();
-            result.next();
-            return new Facility
-                (
-                    result.getInt("facilityid"),
-                    result.getString("name")
-                );
-        }
-        catch (SQLException throwables)
-        {
-            throw new IllegalArgumentException(throwables.getMessage());
-        }
-    }
+    return null;
+  }
 
-    private List<Facility> getAllFacilities()
+  private List<Rule> getRulesByResidenceId(int residenceId)
+  {
+    List<Rule> ruleListToReturn = new ArrayList<>();
+    try (Connection connection = getConnection())
     {
-        List<Facility> facilityListToReturn = new ArrayList<>();
-        try(Connection connection = getConnection())
-        {
-            PreparedStatement stm = connection.prepareStatement
-                ("SELECT * FROM facility");
-            ResultSet result = stm.executeQuery();
-            while (result.next())
-            {
-                Facility facility = new Facility
-                    (
-                        result.getInt("facilityid"),
-                        result.getString("name")
-                    );
-                facilityListToReturn.add(facility);
-            }
-            return facilityListToReturn;
-        }
-        catch (SQLException throwables)
-        {
-            throw new IllegalStateException(throwables.getMessage());
-        }
+      PreparedStatement stm = connection.prepareStatement(
+          "SELECT * FROM rule WHERE residenceid = ?");
+      stm.setInt(1, residenceId);
+      ResultSet result = stm.executeQuery();
+      while (result.next())
+      {
+        Rule rule = new Rule(result.getString("ruledescription"),
+            result.getInt("residenceid"));
+        ruleListToReturn.add(rule);
+      }
+      return ruleListToReturn;
     }
+    catch (SQLException throwables)
+    {
+      throw new IllegalArgumentException(throwables.getMessage());
+    }
+  }
+
+  private Facility getFacilityById(int facilityId)
+  {
+    try (Connection connection = getConnection())
+    {
+      PreparedStatement stm = connection.prepareStatement(
+          "SELECT * FROM facility WHERE facilityid = ?");
+      stm.setInt(1, facilityId);
+      ResultSet result = stm.executeQuery();
+      if (result.next())
+      {
+        return new Facility(result.getInt("facilityid"),
+            result.getString("name"));
+      }
+      throw new IllegalArgumentException("Facility cant be null");
+    }
+    catch (SQLException throwables)
+    {
+      throw new IllegalArgumentException(throwables.getMessage());
+    }
+  }
+
+  private List<Facility> getAllFacilities()
+  {
+    List<Facility> facilityListToReturn = new ArrayList<>();
+    try (Connection connection = getConnection())
+    {
+      PreparedStatement stm = connection.prepareStatement(
+          "SELECT * FROM facility");
+      ResultSet result = stm.executeQuery();
+      while (result.next())
+      {
+        Facility facility = new Facility(result.getInt("facilityid"),
+            result.getString("name"));
+        facilityListToReturn.add(facility);
+      }
+      return facilityListToReturn;
+    }
+    catch (SQLException throwables)
+    {
+      throw new IllegalStateException(throwables.getMessage());
+    }
+  }
 }
