0bdb26774a6e573c770a8ac4b04e9f593ca8e24f
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-11-27 18:20:15 +0100

    missing RestController annotation for RentRequestController added

--- t3/viabnb.tier3.dataserver.rest/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/controllers/RentRequestController.java
+++ t3/viabnb.tier3.dataserver.rest/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/controllers/RentRequestController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RestController
 public class RentRequestController {
     private RentRequestDAO rentRequestDAO;
 
