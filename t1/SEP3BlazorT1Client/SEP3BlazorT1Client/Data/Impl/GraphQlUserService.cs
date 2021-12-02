585274ee08d558d002698690ca892ca2786f0b45
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-12-02 21:57:21 +0100

    User service added to T1

--- t1/SEP3BlazorT1Client/SEP3BlazorT1Client/Data/Impl/GraphQlUserService.cs
+++ t1/SEP3BlazorT1Client/SEP3BlazorT1Client/Data/Impl/GraphQlUserService.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using CatQL.GraphQL.Client;
+using CatQL.GraphQL.QueryResponses;
+using Newtonsoft.Json;
+using SEP3BlazorT1Client.Data.Impl.ResponseTypes;
 using SEP3BlazorT1Client.Models;
 
 namespace SEP3BlazorT1Client.Data.Impl
@@ -10,24 +14,82 @@ namespace SEP3BlazorT1Client.Data.Impl
         private const string Url = "https://localhost:5001/graphql";
         private readonly GqlClient _client = new GqlClient(Url) {EnableLogging = true};
         
-        public Task<User> GetUserByEmailAsync(string email)
+        public async Task<User> GetUserByEmailAsync(string email)
         {
-            throw new System.NotImplementedException();
+            var query = new GqlQuery()
+            {
+                Query = @"query($userEmail:String){
+                              userByEmail(email:$userEmail){
+                                id
+                                email
+                                password
+                                firstName
+                                lastName
+                                phoneNumber
+                              }
+                            }",
+                Variables = new {userEmail = email}
+            };
+            var response = await _client.PostQueryAsync<UserByEmailResponseType>(query);
+            HandleErrorResponse(response);
+            return response.Data.User;
         }
 
-        public Task<User> GetUserByIdAsync(int id)
+        public async Task<User> GetUserByIdAsync(int id)
         {
-            throw new System.NotImplementedException();
+            var query = new GqlQuery()
+            {
+                Query = @"query($userId:Int!){
+                              userById(id:$userId){
+                                id
+                                email
+                                password
+                                firstName
+                                lastName
+                                phoneNumber
+                              }
+                            }",
+                Variables = new {userId = id}
+            };
+            var response = await _client.PostQueryAsync<UserByIdResponseType>(query);
+            HandleErrorResponse(response);
+            return response.Data.User;
         }
 
-        public Task<IEnumerable<User>> GetAllUsersAsync()
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
         {
-            throw new System.NotImplementedException();
+            var query = new GqlQuery()
+            {
+            Query = @"query{
+                        allUsers{
+                           id
+                           email
+                           password
+                           firstName
+                           lastName
+                           phoneNumber
+                         }
+                       }"
+        };
+        var response = await _client.PostQueryAsync<AllUsersResponseType>(query);
+        HandleErrorResponse(response);
+            return response.Data.Users;
         }
 
         public Task<User> ValidateUserAsync(string email, string password)
         {
             throw new System.NotImplementedException();
+        }
+        
+        private static void HandleErrorResponse<T>(GqlRequestResponse<T> response)
+        {
+            if (response.Errors != null)
+            {
+                // String manipulation to seperate the Error message from the sample error response. 
+                Console.WriteLine(JsonConvert.SerializeObject(response.Errors));
+                throw new ArgumentException(JsonConvert.SerializeObject(response.Errors).Split(",")[4]
+                    .Split(":")[2]);
+            }
         }
     }
 }
--- t1/SEP3BlazorT1Client/SEP3BlazorT1Client/Data/Impl/ResponseTypes/UserByIdResponseType.cs
+++ t1/SEP3BlazorT1Client/SEP3BlazorT1Client/Data/Impl/ResponseTypes/UserByIdResponseType.cs
@@ -3,8 +3,8 @@ using SEP3BlazorT1Client.Models;
 
 namespace SEP3BlazorT1Client.Data.Impl.ResponseTypes
 {
-    public class UserByEmailResponseType
+    public class UserByIdResponseType
     {
-        [JsonProperty("userByEmail")] public User User { get; set; }
+        [JsonProperty("userById")] public User User { get; set; }
     }
 }
