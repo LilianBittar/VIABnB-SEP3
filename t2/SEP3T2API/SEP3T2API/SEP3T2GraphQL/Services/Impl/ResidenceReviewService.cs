f14540a85b77381ce7643a107e0edd373a2830fd
Michael Bui <michaelbui99@hotmail.com>
2021-12-03 13:11:50 +0100

    Added average rating to ResidenceCard

--- t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/Impl/ResidenceReviewService.cs
+++ t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/Impl/ResidenceReviewService.cs
@@ -1,21 +1,27 @@
 ﻿using System.Net.Http;
 using System.Threading.Tasks;
 using SEP3T2GraphQL.Models;
+using SEP3T2GraphQL.Repositories;
+using SEP3T2GraphQL.Services.Validation;
 
 namespace SEP3T2GraphQL.Services.Impl
 {
     public partial class ResidenceReviewService : IResidenceReviewService
     {
         private readonly IResidenceService _residenceService;
+        private readonly IResidenceReviewRepository _residenceReviewRepository;
+        private readonly CreateResidenceReviewValidator _validator; 
 
-        public ResidenceReviewService(IResidenceService residenceService)
+        public ResidenceReviewService(IResidenceService residenceService, IResidenceReviewRepository residenceReviewRepository, CreateResidenceReviewValidator validator)
         {
             _residenceService = residenceService;
+            _residenceReviewRepository = residenceReviewRepository;
+            _validator = validator; 
         }
-
         public Task<ResidenceReview> CreateAsync(Residence residence, ResidenceReview residenceReview)
         {
-            throw new System.NotImplementedException();
+            _validator.ValidateResidenceReview(residenceReview);
+            
         }
     }
 }
---
+++ t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/Validation/CreateResidenceReviewValidator.cs
@@ -0,0 +1,22 @@
+﻿using SEP3T2GraphQL.Models;
+using SEP3T2GraphQL.Repositories;
+
+namespace SEP3T2GraphQL.Services.Validation
+{
+    public class CreateResidenceReviewValidator
+    {
+        private readonly IResidenceService _residenceService;
+        private readonly IRentRequestRepository _rentRequestRepository;
+
+        public CreateResidenceReviewValidator(IResidenceService residenceService, IRentRequestRepository rentRequestRepository)
+        {
+            _residenceService = residenceService;
+            _rentRequestRepository = rentRequestRepository;
+        }
+
+        public void ValidateResidenceReview(ResidenceReview residenceReview)
+        {
+            
+        }
+    }
+}
