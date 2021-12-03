36ca8bbea1ac9b28afe6c5b4e2bb49d953d5a2a1
Michael Bui <michaelbui99@hotmail.com>
2021-12-03 16:47:57 +0100

    Implemented create in ResidenceReviewDAO

--- t3/viabnb.tier3.dataserver.rest/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/dao/residencereview/ResidenceReviewDAOImpl.java
+++ t3/viabnb.tier3.dataserver.rest/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/dao/residencereview/ResidenceReviewDAOImpl.java
@@ -4,19 +4,26 @@ import dk.viabnb.sep3.group6.dataserver.rest.t3.dao.BaseDao;
 import dk.viabnb.sep3.group6.dataserver.rest.t3.models.Residence;
 import dk.viabnb.sep3.group6.dataserver.rest.t3.models.ResidenceReview;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 public class ResidenceReviewDAOImpl extends BaseDao implements ResidenceReviewDAO{
     @Override
     public ResidenceReview create(Residence residence, ResidenceReview residenceReview) {
         try (Connection connection = getConnection()) {
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO residencereview(residencerating, residencereviewtext, residenceid, guestid, createddate) values (?,?,?,?,?)");
+            stm.setDouble(1, residenceReview.getRating());
+            stm.setString(2, residenceReview.getReviewText());
+            stm.setInt(3, residence.getId());
+            stm.setInt(4,getGuestIdByViaId(residenceReview.getGuestViaId()));
+            stm.setDate(5, Date.valueOf(residenceReview.getCreatedDate()));
+            stm.executeUpdate();
+            connection.commit();
+            return residenceReview;
         } catch (SQLException throwables) {
             throwables.printStackTrace();
             throw new IllegalStateException(throwables.getMessage());
         }
-        return null;
     }
 
     @Override
@@ -27,5 +34,19 @@ public class ResidenceReviewDAOImpl extends BaseDao implements ResidenceReviewDA
     @Override
     public List<ResidenceReview> getAllByResidenceId(int id) {
         return null;
+    }
+
+
+    private int getGuestIdByViaId(int viaId){
+        try (Connection connection = getConnection()){
+            PreparedStatement stm = connection.prepareStatement("select guestid from guest where viaid = ?");
+            stm.setInt(1, viaId);
+            ResultSet result = stm.executeQuery();
+            result.next();
+            return result.getInt(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            throw new IllegalStateException(throwables.getMessage());
+        }
     }
 }
