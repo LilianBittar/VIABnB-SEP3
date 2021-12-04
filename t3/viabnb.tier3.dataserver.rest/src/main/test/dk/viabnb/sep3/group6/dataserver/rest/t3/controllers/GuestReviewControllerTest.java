f944c8d18dc1cebc021f1255dcfc19b5adbaef4b
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-12-04 18:15:47 +0100

    setup empty test for GuestReviewController

---
+++ t3/viabnb.tier3.dataserver.rest/src/main/test/dk/viabnb/sep3/group6/dataserver/rest/t3/controllers/GuestReviewControllerTest.java
@@ -0,0 +1,31 @@
+package dk.viabnb.sep3.group6.dataserver.rest.t3.controllers;
+
+import dk.viabnb.sep3.group6.dataserver.rest.t3.dao.guestreview.GuestReviewDAO;
+import dk.viabnb.sep3.group6.dataserver.rest.t3.models.GuestReview;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.LocalDate;
+
+import static org.mockito.Mockito.mock;
+
+class GuestReviewControllerTest
+{
+  private GuestReviewDAO guestReviewDAO;
+  private GuestReviewController guestReviewController;
+  private GuestReview guestReview;
+
+  @BeforeEach
+  public void setUp()
+  {
+    guestReviewDAO = mock(GuestReviewDAO.class);
+    guestReviewController = new GuestReviewController(guestReviewDAO);
+    guestReview = new GuestReview
+        (
+          3.5,
+          "Test",
+          "Test@test.tt",
+          LocalDate.now()
+        );
+  }
+
+}
