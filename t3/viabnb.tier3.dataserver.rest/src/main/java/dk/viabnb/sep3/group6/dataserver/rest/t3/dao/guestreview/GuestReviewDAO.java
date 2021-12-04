b6a54db46a31d48982ba16c305400acbda5350e2
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-12-04 18:47:52 +0100

    java docs to T3 GuestReviewDAO

--- t3/viabnb.tier3.dataserver.rest/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/dao/guestreview/GuestReviewDAO.java
+++ t3/viabnb.tier3.dataserver.rest/src/main/java/dk/viabnb/sep3/group6/dataserver/rest/t3/dao/guestreview/GuestReviewDAO.java
@@ -6,7 +6,30 @@ import java.util.List;
 
 public interface GuestReviewDAO
 {
+  /**
+   * Method to create a new GuestReview object and store it in a database
+   *
+   * @param guestReview The new object
+   * @return newly created GuestReview object
+   *
+   * @throws IllegalArgumentException On an invalid Parameter
+   * */
   GuestReview createGuestReview(GuestReview guestReview);
+  /**
+   * Method to update a GuestReview object and store it in a database
+   *
+   * @param guestReview The new updated object
+   * @return newly updated GuestReview object
+   *
+   * @throws IllegalArgumentException On an invalid Parameter
+   * */
   GuestReview updateGuestReview(GuestReview guestReview);
+  /**
+   * Method that query a list of guest reviews
+   * @param id The id of the guest whom the reviews belongs to
+   * @return A list of GuestReview objects with a guest id that matches the given parameter
+   *
+   * @throws IllegalArgumentException if the given parameter is invalid
+   * */
   List<GuestReview> getAllGuestReviewsByGuestId(int id);
 }
