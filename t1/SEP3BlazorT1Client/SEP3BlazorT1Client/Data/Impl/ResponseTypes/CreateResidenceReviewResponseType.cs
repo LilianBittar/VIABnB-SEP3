7819087976bfff16a904e80b2e78d4901342a395
Michael Bui <michaelbui99@hotmail.com>
2021-12-05 16:09:58 +0100

    Implemented mutation for CreateResidenceReview in t1 ResidenceReview service

---
+++ t1/SEP3BlazorT1Client/SEP3BlazorT1Client/Data/Impl/ResponseTypes/CreateResidenceReviewResponseType.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using SEP3BlazorT1Client.Models;
+
+namespace SEP3BlazorT1Client.Data.Impl.ResponseTypes
+{
+    public class CreateResidenceReviewResponseType
+    {
+        [JsonProperty("createResidenceReview")]
+        public ResidenceReview ResidenceReview { get; set; }
+    }
+}
