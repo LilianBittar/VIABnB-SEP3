9fdd75d60a1665bbad2fde759bd39f27f2f51871
Michael Bui <michaelbui99@hotmail.com>
2021-12-08 12:03:58 +0100

    Updated DB DDL with messages

--- t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/Impl/MessagingService.cs
+++ t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/Impl/MessagingService.cs
@@ -42,6 +42,7 @@ namespace SEP3T2GraphQL.Services.Impl
 
             try
             {
+                message.TimeSent = DateTime.Now;
                 var newMessage = await _messageRepository.CreateMessageAsync(message);
                 // Adding sender / receiver to map with a new message queue in case they have never sent / received a message before. 
                 if (!_messageMap.ContainsKey(message.Receiver.Id))
