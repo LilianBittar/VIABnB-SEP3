dc005c3ccb45d16e5d9465225d927274e04f6c87
Kasper <kasperscoop@gmail.com>
2021-12-10 17:59:59 +0100

    added password switch to login

--- t1/SEP3BlazorT1Client/SEP3BlazorT1Client/Pages/UserLogin/Login.cs
+++ t1/SEP3BlazorT1Client/SEP3BlazorT1Client/Pages/UserLogin/Login.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using MatBlazor;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using SEP3BlazorT1Client.Authentication;
@@ -12,65 +13,63 @@ namespace SEP3BlazorT1Client.Pages.UserLogin
         [Inject] public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
         [Inject] public NavigationManager NavigationManager { get; set; }
         [Inject] public IGuestService GuestService { get; set; }
-        
+
         private string email;
-            private string password;
-            private string ErrorMessage;
-            private bool loginAsHost;
-            private int studentNumber;
-            public string TxtType = "password"; 
-            private bool IsShow {get;set;} = true;
-            private void ShowLoadingBar()
+        private string password;
+        private string ErrorMessage;
+        private bool loginAsHost;
+        private string TxtType = "password";
+        private string IconType = MatIconNames.Visibility_off;
+        private bool IsShow { get; set; } = true;
+
+        private void ShowLoadingBar()   
+        {
+            IsShow = !IsShow;
+        }
+
+        public void ShowPassword()
+        {
+            if (this.TxtType == "password")
             {
-                IsShow =   !IsShow;
-            }  
-        
-            public void ShowPassword() 
-            { 
-                if(this.TxtType== "password") 
-                { 
-                    this.TxtType = "text"; 
-                } 
-                else 
-                { 
-                    this.TxtType = "password"; 
-                } 
-            } 
-            
-            private async Task PerformLogin()
+                this.TxtType = "text";
+                IconType = MatIconNames.Visibility;
+            }
+            else
             {
-              
-                ErrorMessage = "";
-                try
-                {
-                    if (loginAsHost)
-                    {
-                        ShowLoadingBar();
-                        await ((CustomAuthenticationStateProvider) AuthenticationStateProvider).ValidateLogin( email, password);
-                        password = "";
-                        studentNumber = 0;
-                        NavigationManager.NavigateTo("/HostResidences");
-                    }
-                    else
-                    {
-                        ShowLoadingBar();
-                        await ((CustomAuthenticationStateProvider) AuthenticationStateProvider).ValidateLogin(email, password);
-                        email = "";
-                        password = "";
-                        NavigationManager.NavigateTo("/User");
-                    }
-                }
-                catch (Exception e)
+                this.TxtType = "password";
+                IconType = MatIconNames.Visibility_off;
+            }
+        }
+
+        private async Task PerformLogin()
+        {
+            ErrorMessage = "";
+            try
+            {
+                if (loginAsHost)
                 {
                     ShowLoadingBar();
-                    ErrorMessage = e.Message;
-                    Console.WriteLine(e.Message);
+                    await ((CustomAuthenticationStateProvider) AuthenticationStateProvider).ValidateLogin(email,
+                        password);
+                    password = "";
+                    NavigationManager.NavigateTo("/HostResidences");
+                }
+                else
+                {
+                    ShowLoadingBar();
+                    await ((CustomAuthenticationStateProvider) AuthenticationStateProvider).ValidateLogin(email,
+                        password);
+                    email = "";
+                    password = "";
+                    NavigationManager.NavigateTo("/User");
                 }
             }
-        
-            private string getLabel(bool val)
+            catch (Exception e)
             {
-                return !val ? "Switch to guest login" : "Switch to host login";
+                ShowLoadingBar();
+                ErrorMessage = e.Message;
+                Console.WriteLine(e.Message);
             }
+        }
     }
 }
