1afbdae5f867d894c4ab030c8a2be5542d438b84
Lil <bittarlily@gmail.com>
2021-12-11 14:52:13 +0100

    implemented test for creating a guest review

---
+++ t2/SEP3T2API/SEP3T2API/UnitTests/GuestReviewHostServiceTest/CreateGuestReviewTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using SEP3T2GraphQL.Models;
+using SEP3T2GraphQL.Repositories;
+using SEP3T2GraphQL.Services.Impl;
+using SEP3T2GraphQL.Services.Validation;
+
+namespace UnitTests.GuestReviewHostServiceTest
+{
+    [TestFixture]
+    public class CreateGuestReviewTest
+    {
+        private GuestReview _guestReview;
+        private Mock<IGuestReviewHostRepository> _guestReviewHostRepository;
+        private CreateGuestReviewValidation _validator;
+        private GuestReviewService _guestReviewService;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _guestReview = new GuestReview()
+            {
+                CreatedDate = new DateTime(),
+                GuestId = 1,
+                HostId = 3,
+                Rating = 4.0,
+                Text = "Was oki doki."
+            };
+    
+            _guestReviewHostRepository = new Mock<IGuestReviewHostRepository>();
+            _validator = new CreateGuestReviewValidation(_guestReviewHostRepository.Object);
+            _guestReviewService = new GuestReviewService(_guestReviewHostRepository.Object, _validator);
+        }
+        
+        
+        [Test]
+        [TestCase(-1)]
+        [TestCase(-10)]
+        [TestCase(-100)]
+        [TestCase(6)]
+        [TestCase(10)]
+        [TestCase(100)]
+        public void CreateGuestReview_InvalidRating_ThrowsArgumentException(int rating)
+        {
+            GuestReview guestReview = new GuestReview()
+            {
+                CreatedDate = new DateTime(),
+                GuestId = 1,
+                HostId = 3,
+                Rating = rating,
+                Text = "Was oki doki."
+            };
+            Assert.ThrowsAsync<ArgumentException>(async () => 
+                await _guestReviewService.CreateGuestReviewAsync(guestReview));
+        }
+        
+        
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void CreateGuestReview_ValidRating_DoesNotThrow(int rating)
+        {
+            GuestReview guestReview = new GuestReview()
+            {
+                CreatedDate = new DateTime(),
+                GuestId = 1,
+                HostId = 3,
+                Rating = rating,
+                Text = "Was oki doki."
+            };
+            Assert.DoesNotThrowAsync(async () => 
+                await _guestReviewService.CreateGuestReviewAsync(guestReview));
+        }
+    }
+}
