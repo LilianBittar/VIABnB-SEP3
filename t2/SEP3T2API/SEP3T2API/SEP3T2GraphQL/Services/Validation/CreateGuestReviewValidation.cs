c07ec32174c84439a8eba88eef6f74eb0f1be492
Michael Bui <michaelbui99@hotmail.com>
2021-12-13 18:38:11 +0100

    Added new validation for CreateHostReviewValidation + documentation

--- t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/Validation/CreateGuestReviewValidation.cs
+++ t2/SEP3T2API/SEP3T2API/SEP3T2GraphQL/Services/Validation/CreateGuestReviewValidation.cs
@@ -29,6 +29,11 @@ namespace SEP3T2GraphQL.Services.Validation
             await ValidateGuestHasRentResidenceOfHost(guestReview);
         }
 
+        /// <summary>
+        /// Validates if the <c>Rating</c> of <c>hostReview</c> is between 0 or 5
+        /// </summary>
+        /// <param name="hostReview">review being validated</param>
+        /// <exception cref="ArgumentException">if rating is not between 0 or 5</exception>
         private static void ValidateRating(GuestReview guestReview)
         {
             if (guestReview.Rating is < 0 or > 5)
@@ -45,8 +50,13 @@ namespace SEP3T2GraphQL.Services.Validation
         /// <exception cref="ArgumentException">If guest has no approved <c>RentRequest</c> for a residence owned by host of the <c>GuestReview</c></exception>
         private async Task ValidateGuestHasRentResidenceOfHost(GuestReview guestReview)
         {
-            var allRentRequests = await _rentalService.GetRentRequestsByGuestIdAsync(guestReview.GuestId);
-            var approvedRequestsHostIsOwner = allRentRequests.Where(r =>
+            var allGuestRentRequests = await _rentalService.GetRentRequestsByGuestIdAsync(guestReview.GuestId);
+            var guestRentRequests = allGuestRentRequests.ToList();
+            if (!guestRentRequests.Any())
+            {
+                throw new ArgumentException("Guest has never rented any of host's residences before");
+            }
+            var approvedRequestsHostIsOwner = guestRentRequests.Where(r =>
                 r.Residence.Host.Id == guestReview.HostId && r.Status == RentRequestStatus.APPROVED);
             if (!approvedRequestsHostIsOwner.Any())
             {
