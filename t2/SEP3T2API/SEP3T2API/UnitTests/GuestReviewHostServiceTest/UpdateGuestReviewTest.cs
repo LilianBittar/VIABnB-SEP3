e6ac494cf54805358264ba834cb10a8eedd86ddf
Lil <bittarlily@gmail.com>
2021-12-13 13:35:45 +0100

    miaw

--- t2/SEP3T2API/SEP3T2API/UnitTests/GuestReviewHostServiceTest/UpdateGuestReviewTest.cs
+++ t2/SEP3T2API/SEP3T2API/UnitTests/GuestReviewHostServiceTest/UpdateGuestReviewTest.cs
@@ -30,5 +30,47 @@ namespace UnitTests.GuestReviewHostServiceTest
             Assert.ThrowsAsync<ArgumentException>(async () =>
                 await _guestReviewService.UpdateGuestReviewAsync(guestReview));
         }
+        
+        
+        [Test]
+        [TestCase(-1)]
+        [TestCase(-10)]
+        [TestCase(-100)]
+        [TestCase(6)]
+        [TestCase(10)]
+        [TestCase(100)]
+        public void UpdateGuestReview_InvalidRating_ThrowsArgumentException(int rating)
+        {
+            GuestReview guestReview = new GuestReview()
+            {
+                CreatedDate = new DateTime(),
+                GuestId = 1,
+                HostId = 3,
+                Rating = rating,
+                Text = "Was oki doki."
+            };
+            Assert.ThrowsAsync<ArgumentException>(async () => 
+                await _guestReviewService.UpdateGuestReviewAsync(guestReview));
+        }
+        
+        
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void UpdateGuestReview_ValidRating_DoesNotThrow(int rating)
+        {
+            GuestReview guestReview = new GuestReview()
+            {
+                CreatedDate = new DateTime(),
+                GuestId = 1,
+                HostId = 3,
+                Rating = rating,
+                Text = "Was oki doki."
+            };
+            Assert.DoesNotThrowAsync(async () => 
+                await _guestReviewService.UpdateGuestReviewAsync(guestReview));
+        }
     }
 }
